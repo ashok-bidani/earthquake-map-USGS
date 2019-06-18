cf622d3589ff9240e3d30db79b9db09ea3cd00a3
Ashok Bidani <ashok.bidani.321@gmail.com>
2019-06-18 16:49:07 -0500

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,61 +1,69 @@
-// Javascript Code to create a Leaflet-based earthquake map. The code uses D3 to request and store data, and creates a Leaflet map with baseMap and overlayMap layers. The base map comes from ESRI and the overlay is earthquake incidences (represented by Leaflet circle markers) using USGS earthquake data. With the earthquakes, incidences are color coded by eathquake scale and the size of the circle marker also changes based on earthquake scale.
+// Javascript Code to create a Leaflet-based earthquake map. The code uses D3 to request and add data, and creates a Leaflet map with baseMap and overlayMap layers. The base map comes from ESRI and the overlay is earthquake incidences (represented by Leaflet circle markers) using USGS earthquake data. With the earthquakes, incidences are color coded by eathquake scale and the size of the circle marker also changes based on earthquake scale.
 
-// Store the USGS geojson earthquake data in the queryUrl variable
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_month.geojson"
 
-/*// Using D3, perform a request to the USGS earthquake data
-d3.json(queryUrl, function(data) {
-  // Once we get a response, send data.features to the createFeatures function
-  createFeatures(data.features);
-});*/
+// First create and initialize the map where the map element ('mapdiv') is
+var myMap = L.map('mapdiv', {center: [37.09, -95.71], zoom: 5});
+    
+var streetmap = L.tileLayer.provider('Esri.WorldStreetMap', {attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012', maxZoom: 18});
+myMap.addLayer(streetmap);
 
-/*function createFeatures(earthquakeData) {
+// Using D3, place a call to the USGS earthquake geoJSON data
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_week.geojson", function(data) {
 
-  // The following function runs once for each feature, describing the earthquake incidence time and place
-  function onEachFeature(feature, layer) {
-    layer.bindPopup("<h3>" + feature.properties.place +
-      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+  // This function returns the style data for each of the earthquakes we plot on
+  // the map. We pass the magnitude of the earthquake into two separate functions
+  // to calculate the color and radius.
+  function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: getColor(feature.properties.mag),
+      color: "#000000",
+      radius: getRadius(feature.properties.mag),
+      stroke: true,
+      weight: 0.5
+    };
   }
 
-  // Now we create a GeoJSON layer containing the earthquake data, and pass it to the createMap function 
-  var earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature
-  });
+  // This function determines the color of the marker based on the magnitude of the earthquake.
+  function getColor(magnitude) {
+    switch (true) {
+    case magnitude > 5:
+      return "#ea2c2c";
+    case magnitude > 4:
+      return "#ea822c";
+    case magnitude > 3:
+      return "#ee9c00";
+    case magnitude > 2:
+      return "#eecc00";
+    case magnitude > 1:
+      return "#d4ee00";
+    default:
+      return "#98ee00";
+    }
+  }
 
-  createMap(earthquakes);
-}*/
+  // This function determines the radius of the earthquake marker based on its magnitude.
+  // Earthquakes with a magnitude of 0 were being plotted with the wrong radius.
+  function getRadius(magnitude) {
+    if (magnitude === 0) {
+      return 1;
+    }
 
-function createMap() {
-    
-    // Initialize map
-    var myMap = L.map('mapdiv', {center: [37.09, -95.71], zoom: 5});
-    
-    // Define streetmap
-    var streetmap = L.tileLayer.provider('Esri.WorldStreetMap', {attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012',
-    maxZoom: 18});
-    
-    // Add streetmap to map
-    myMap.addLayer(streetmap);
+    return magnitude * 4;
+  }
 
-  /*// Define a baseMaps object to hold our base layers
-  var baseMaps = {
-    "Street Map": streetmap
-  };*/
-
-  /*// Create overlay object to hold our overlay layer
-  var overlayMaps = {
-    Earthquakes: earthquakes
-  };*/
-
-/*  // Create our map, giving it the streetmap and earthquakes layers to display on load
-
-
-  // Create a layer control
-  // Pass in our baseMaps and overlayMaps
-  // Add the layer control to the map
-  L.control.layers(baseMaps, {
-    collapsed: false
-  }).addTo(myMap);*/
-};
-
-createMap();
+  // Here we add a GeoJSON layer to the map once the file is loaded.
+  L.geoJson(data, {
+    // We turn each feature into a circleMarker on the map.
+    pointToLayer: function(feature, latlng) {
+      return L.circleMarker(latlng);
+    },
+    // We set the style for each circleMarker using our styleInfo function.
+    style: styleInfo,
+    // We create a popup for each marker to display the magnitude and location of the earthquake after the marker has been created and styled
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+    }
+  }).addTo(myMap);
+});
