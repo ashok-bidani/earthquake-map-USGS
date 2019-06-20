0178f329f66866e2dd82cb981c0f07e90e54d156
Ashok Bidani <ashok.bidani.321@gmail.com>
2019-06-20 16:21:39 -0500

    Final edit to code

--- logic.js
+++ logic.js
@@ -1,4 +1,4 @@
-// Javascript Code to create a Leaflet-based earthquake map. The code uses D3 to request and add data, and creates a Leaflet map with baseMap and overlayMap layers. The base map comes from ESRI and the overlay is earthquake incidences (represented by Leaflet circle markers) using USGS earthquake data. With the earthquakes, incidences are color coded by eathquake scale and the size of the circle marker also changes based on earthquake scale. Finally, there is also a layer showing
+// Javascript Code to create a Leaflet-based earthquake map. The code uses D3 to request and add data, and creates a Leaflet map with baseMap and overlayMap layers. The base map comes from ESRI and the overlay is earthquake incidences (represented by Leaflet circle markers) using USGS earthquake data. With the earthquakes, incidences are color coded by eathquake scale and the size of the circle marker also changes based on earthquake scale. Finally, there is also a layer showing tectonic plates which makes it possible to compare earthquake locations to existing tectonic plates.
 
 
 // Add three map layers: World street map, world imagery, and world terrain map. The World Imagery map is a nice satellite view that will be used for the default.    
@@ -87,33 +87,6 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_week.geoj
     
     // Now add the earthquakes layer to the map
     earthquakes.addTo(myMap);
-
-    // The last aspect relating to the earthquake data is a legend that tells the user which color is used for each magnitude. This is very similar to the example here: https://leafletjs.com/examples/choropleth/
-    var legend = L.control({position: "bottomright"});
-
-    legend.onAdd = function(map) {
-        var div = L.DomUtil.create("div", "info legend");
-        var grades = [0, 1, 2, 3, 4, 5];
-        var colors = [
-            "#98ee00",
-            "#d4ee00",
-            "#eecc00",
-            "#ee9c00",
-            "#ea822c",
-            "#ea2c2c"
-        ];
-
-        for (var i = 0; i < grades.length; i++) {
-            div.innerHTML +=
-                "<i style='background: " + colors[i] + "></i> " +
-                grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
-        }
-        
-        return div;
-    };
-
-    // Have to add the legend to the map.
-    legend.addTo(myMap);
     
     // Now do the same thing for the tectonic plate data. Here I have stored it in a data file, rather than accessing it externally from the web
     d3.json("data/PB2002_boundaries.json").then(function(platedata) {
